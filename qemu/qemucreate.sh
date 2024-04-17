c7cd9f73693b091494a9f3a2e5b945bdda202804
h0lik <sorax.t@hotmail.com>
2024-04-17 22:01:14 +0400

    First Commit

---
+++ qemu/qemucreate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#==================================================
+# Name:Qemu-img create
+# Autor:H0lik@linux
+# Github:github.com/h0lik
+# Date:17.04.2024
+#==================================================
+dir=/mnt/hdd/vmhub/
+
+echo -e "Hello \e[32m $USER \e[0m ,Creating a virtual machine disk?"
+read -p "-Format virtual disk:" Hard
+read -p "-Name virtual disk:" Name
+read -p "-Size virtual disk:" Size
+sudo qemu-img create -f $Hard $dir$Name.$Hard $Size
+echo -e "Virtual disk:$name format:$Hard Size:$Size >>>>>>>  \e[32m Created! \e[0m "
