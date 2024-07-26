a3ab10cae053d44439f6c10098d9abffa9fedf1f
N0lik <150377744+h0lik@users.noreply.github.com>
2024-07-26 16:25:24 +0400

    Update installrpmos.sh

--- base-os/installrpmos.sh
+++ base-os/installrpmos.sh
@@ -2,7 +2,7 @@
 
 echo -e "\e[32m Install base software rpm OS! \e[0m"
 sleep 2
-sudo dnf update
+sudo dnf update -y
 sudo dnf -y install epel-release
 sudo dnf update
 sudo dnf -y install wget libselinux libselinux-utils mc git nmap traceroute tcpdump bind-utils net-tools cpan ipset conntrack iptables iptables-service htop nmon atop tree psmisc ncdu ipset-service nfs-utils
