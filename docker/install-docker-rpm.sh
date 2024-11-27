ee6fdfcdba4648841394601165d7bf2e307321c5
Андрей Курилов <h0lik@mac-redfex.local>
2024-11-28 00:29:22 +0400

    edit

--- docker/install-docker-rpm.sh
+++ docker/install-docker-rpm.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 installdocker() {
-    dnf install -y dnf-utils
-    dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+    dnf -y config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
     yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 enabledocker() {
